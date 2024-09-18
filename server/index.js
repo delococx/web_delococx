f3f9a2404e1d0f0ef6febbafd46cf6c5d9f4f06f
delococx <delococx52@gmail.com>
2024-09-18 03:14:04 -0400

    Primer commit con el proyecto inicial

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config()
+
+// Crear una instancia de la aplicación Express
+const app = express();
+
+// Deshabilitar el encabezado 'x-powered-by'
+app.disable('x-powered-by');
+
+// Definir el puerto en el que el servidor escuchará
+const port = process.env.PORT ?? 3000;
+
+// Configurar una ruta básica para la raíz
+app.get("/", (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+ });
+
+// Iniciar el servidor y hacer que escuche en el puerto especificado
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
